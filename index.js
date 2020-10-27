ac60c1afb536ef6088f941c014ff6902a8379c2d
jkugsiya <jkugsiya11@gmail.com>
2020-10-27 21:36:55 +0530

    some changes

---
+++ index.js
@@ -0,0 +1,4 @@
+var hashTags = [
+    "#MaintainsUrbanBiosphere", "#CaptureAndRescueStormWater", "#DesignForAndByPeople", "#DigitalTransformationOfCouncilBureacrats", "#NewAreasOfEmployment", "#PropertyPrice", "#CovidOutbreakGuardRegulation", "#CitizenParticipationInDirectDemocracy", "#AffordableCommunityFacilitiesAndServices", "#SupportHomeless", "#CreativeValue", "#E-learningPolicies", "#CyclistFriendlyCity", "#AffordablePublicTransport", "#EnableCitizenEngagementWithNewTechnology", "#OpenupGovernmentData", "#CityCleanness", "#MoreHotspot", "#AcknowledgeAboriginalIdentity", "#MoreMinorityRights"
+]
+
