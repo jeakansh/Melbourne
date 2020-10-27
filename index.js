23a2b059e15b15424534fa1e7543912ebe2bc87c
jkugsiya <jkugsiya11@gmail.com>
2020-10-27 23:17:36 +0530

    added validation for no duplication of hashtags

--- index.js
+++
@@ -1,4 +0,0 @@
-var hashTags = [
-    "#MaintainsUrbanBiosphere", "#CaptureAndRescueStormWater", "#DesignForAndByPeople", "#DigitalTransformationOfCouncilBureacrats", "#NewAreasOfEmployment", "#PropertyPrice", "#CovidOutbreakGuardRegulation", "#CitizenParticipationInDirectDemocracy", "#AffordableCommunityFacilitiesAndServices", "#SupportHomeless", "#CreativeValue", "#E-learningPolicies", "#CyclistFriendlyCity", "#AffordablePublicTransport", "#EnableCitizenEngagementWithNewTechnology", "#OpenupGovernmentData", "#CityCleanness", "#MoreHotspot", "#AcknowledgeAboriginalIdentity", "#MoreMinorityRights"
-]
-
